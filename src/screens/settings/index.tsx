6ffd71e361a8b88e4334ed0155a87c6ff42314d2
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2021-03-30 21:48:27 -0400

    chore: Add settings screen

---
+++ src/screens/settings/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+export function SettingsScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Settings</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
