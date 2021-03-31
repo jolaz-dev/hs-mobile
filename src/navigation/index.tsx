fbfed31f70cfe599928f462335b2e69474110f17
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2021-03-30 22:02:00 -0400

    chore: Adapt drawer to dark theme

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,12 +3,23 @@ import {NavigationConsts} from '../consts/navigation-consts';
 import {SettingsScreen} from '../screens/settings';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {MainStackNavigator} from './main-stack';
+import {isDarkModeSelected} from '../style/is-dark-mode-selected';
+import {AppColors} from '../style/colors';
 
 const Drawer = createDrawerNavigator();
 
 export const MainNavigator: React.FC = () => {
   return (
-    <Drawer.Navigator initialRouteName={NavigationConsts.MAIN}>
+    <Drawer.Navigator
+      initialRouteName={NavigationConsts.MAIN}
+      drawerStyle={{
+        backgroundColor: isDarkModeSelected() ? AppColors.SpaceCadet : AppColors.Manatee,
+      }}
+      drawerContentOptions={{
+        activeBackgroundColor: isDarkModeSelected() ? AppColors.Manatee : AppColors.AliceBlue,
+        activeTintColor: isDarkModeSelected() ? AppColors.AliceBlue : AppColors.Gunmetal,
+        inactiveTintColor: isDarkModeSelected() ? AppColors.Manatee : AppColors.Gunmetal,
+      }}>
       <Drawer.Screen name={NavigationConsts.MAIN} component={MainStackNavigator} />
       <Drawer.Screen
         name={NavigationConsts.SETTINGS}
